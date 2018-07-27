299a11a175d582ee4f728679075ba5c22cc3f697
hoodielive <hoodielive@gmail.com>
2018-07-27 07:58:05 -0400

    get input from user

--- blockchains_are_lists_of_hashes_of_transactions.py
+++ blockchains_are_lists_of_hashes_of_transactions.py
@@ -6,8 +6,10 @@ def get_last_blockchain_value():
 def add_value(transaction_amount, last_transaction=1):
     blockchain.append([last_transaction,transaction_amount]) 
 
+tx_amount = input('Your transaction amount please: ') 
+add_value(tx_amount)
 add_value(2)
-add_value(0.9, get_last_blockchain_value())
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=0.0) 
 add_value(10.89, get_last_blockchain_value())
 
 print(blockchain) 
