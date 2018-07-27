ee949e859328613069f7be0b41430402ab46158c
hoodielive <hoodielive@gmail.com>
2018-07-27 07:46:40 -0400

    expanded blockchain

--- blockchains_are_lists_of_hashes_of_transactions.py
+++ blockchains_are_lists_of_hashes_of_transactions.py
@@ -1,14 +1,13 @@
 blockchain = [1] 
 
-def add_value():
-    blockchain.append([blockchain[-1], 5.3]) 
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+def add_value(transaction_amount):
+    blockchain.append([get_last_blockchain_value(), transaction_amount]) 
     print(blockchain)
 
 
-add_value()
-add_value()
-add_value()
-add_value()
-add_value()
-add_value()
-add_value()
+add_value(2)
+add_value(0.9)
+add_value(10.89)
