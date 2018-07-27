4ab86d219d5acd6e7db6399eaddac1100766b80d
hoodielive <hoodielive@gmail.com>
2018-07-27 08:05:46 -0400

    DRY -> do not repeat yourself

--- blockchains_are_lists_of_hashes_of_transactions.py
+++ blockchains_are_lists_of_hashes_of_transactions.py
@@ -6,10 +6,15 @@ def get_last_blockchain_value():
 def add_value(transaction_amount, last_transaction=1):
     blockchain.append([last_transaction,transaction_amount]) 
 
-tx_amount = float(input('Your transaction amount please: ')) # remember whatever is passed in to the input() func is 'text'
+def get_user_input():
+    return float(input('Your transaction amount please: ')) 
+
+tx_amount = get_user_input()
+
 add_value(tx_amount)
-add_value(2)
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=0.0) 
-add_value(10.89, get_last_blockchain_value())
+
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount) 
+J
+add_value(tx_amount, get_last_blockchain_value())
 
 print(blockchain) 
