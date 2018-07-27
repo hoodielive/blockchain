ce03d5f9b1b2e06707d27be4e7c41889cc803c80
hoodielive <hoodielive@gmail.com>
2018-07-27 08:01:25 -0400

    everything you pass in must float

--- blockchains_are_lists_of_hashes_of_transactions.py
+++ blockchains_are_lists_of_hashes_of_transactions.py
@@ -6,7 +6,7 @@ def get_last_blockchain_value():
 def add_value(transaction_amount, last_transaction=1):
     blockchain.append([last_transaction,transaction_amount]) 
 
-tx_amount = input('Your transaction amount please: ') 
+tx_amount = float(input('Your transaction amount please: ')) # remember whatever is passed in to the input() func is 'text'
 add_value(tx_amount)
 add_value(2)
 add_value(last_transaction=get_last_blockchain_value(), transaction_amount=0.0) 
